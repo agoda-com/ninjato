4184b93ee9440f1052e1ed05fd679a1860cb9bc7
Ilya Lim <ilya.lim@agoda.com>
2019-02-25 14:52:30 +0700

    Switch from KType to JVM type

--- core/src/main/kotlin/com/agoda/fleksora/serial/Serializer.kt
+++ core/src/main/kotlin/com/agoda/fleksora/serial/Serializer.kt
@@ -1,13 +1,13 @@
 package com.agoda.fleksora.serial
 
 import com.agoda.fleksora.http.Body
-import kotlin.reflect.KType
+import java.lang.reflect.Type
 
 interface Serializer<in I, out O> {
     fun serializer(instance: I): O
 
     interface Factory {
-        fun requestSerializer(type: KType): Serializer<*, Body>
-        fun responseSerializer(type: KType): Serializer<Body, *>
+        fun requestSerializer(type: Type): Serializer<*, Body>
+        fun responseSerializer(type: Type): Serializer<Body, *>
     }
 }
