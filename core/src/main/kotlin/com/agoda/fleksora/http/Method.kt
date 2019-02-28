b541c1a1464494cad6669e57af68521d24fa007d
Ilya Lim <ilya.lim@agoda.com>
2019-02-28 18:31:30 +0700

    Add OkHttpClient implementation

--- core/src/main/kotlin/com/agoda/fleksora/http/Method.kt
+++ core/src/main/kotlin/com/agoda/fleksora/http/Method.kt
@@ -1,11 +1,11 @@
 package com.agoda.fleksora.http
 
-sealed class Method(val requiresBody: Boolean) {
-    object Get : Method(false)
-    object Head : Method(false)
-    object Post : Method(true)
-    object Put : Method(true)
-    object Delete : Method(false)
-    object Options : Method(false)
-    object Patch : Method(true)
+sealed class Method(val name: String, val requiresBody: Boolean) {
+    object Get : Method("get", false)
+    object Head : Method("head", false)
+    object Post : Method("post", true)
+    object Put : Method("put", true)
+    object Delete : Method("delete", false)
+    object Options : Method("options", false)
+    object Patch : Method("patch", true)
 }
