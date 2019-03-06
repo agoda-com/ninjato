7d6783caa181a1be4fe5dcdda495e966178ee192
Dennis <dennis.hsieh@agoda.com>
2019-03-06 17:41:23 +0700

    Return Unit.

--- extensions/extension-rxjava/src/main/kotlin/com/agoda/ninjato/extensions/rxjava/Apis.kt
+++ extensions/extension-rxjava/src/main/kotlin/com/agoda/ninjato/extensions/rxjava/Apis.kt
@@ -5,7 +5,7 @@ import rx.Completable
 import rx.Observable
 import rx.Single
 
-inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> T): Completable = Completable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
 
 inline fun <A : Api, reified T> A.single(noinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
 
--- extensions/extension-rxjava2/src/main/kotlin/com/agoda/ninjato/extensions/rxjava2/Apis.kt
+++ extensions/extension-rxjava2/src/main/kotlin/com/agoda/ninjato/extensions/rxjava2/Apis.kt
@@ -6,7 +6,7 @@ import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
 
-inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> T): Completable = Completable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
 
 inline fun <A : Api, reified T> A.single(noinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
 
