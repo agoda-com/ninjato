0c1c8285f91e212aa645a0e3215a14a2eab90bab
Ilya Lim <ilya.lim@agoda.com>
2019-03-06 17:22:10 +0700

    Add documentation for Commons

--- core/src/main/kotlin/com/agoda/ninjato/dsl/Commons.kt
+++ core/src/main/kotlin/com/agoda/ninjato/dsl/Commons.kt
@@ -8,6 +8,19 @@ import com.agoda.ninjato.policy.Retry
 import com.agoda.ninjato.policy.RetryPolicy
 import com.agoda.ninjato.converter.ConverterFactories
 
+/**
+ * The main DSL entry of the library.
+ *
+ * This interface provides abilities to manipulate [headers][Headers], [interceptors][Interceptors],
+ * [converter factories][ConverterFactories], [retry policies][RetryPolicy] and [fallback policies][FallbackPolicy].
+ *
+ * This interface is reused on 3 different levels:
+ *  - [HttpClient][com.agoda.ninjato.http.HttpClient]
+ *  - [Api][com.agoda.ninjato.Api]
+ *  - [Request.Configurator]
+ *
+ *  This makes the DSL cascade: all the values are aggregated across all three levels to form the final values.
+ */
 interface Commons {
     val headers: Headers
     val interceptors: Interceptors
@@ -16,12 +29,22 @@ interface Commons {
     var retryPolicy: RetryPolicy?
     var fallbackPolicy: FallbackPolicy?
 
+    /**
+     * Creates anonymous implementation of [RetryPolicy] and sets it as current.
+     *
+     * @param policy lambda with retry policy evaluation.
+     */
     fun retryPolicy(policy: (Request, Throwable) -> Retry) {
         retryPolicy = object : RetryPolicy() {
             override fun evaluate(request: Request, throwable: Throwable) = policy(request, throwable)
         }
     }
 
+    /**
+     * Creates anonymous implementation of [FallbackPolicy] and sets it as current.
+     *
+     * @param policy lambda with fallback policy evaluation.
+     */
     fun fallbackPolicy(policy: (Request, Throwable) -> Request) {
         fallbackPolicy = object : FallbackPolicy() {
             override fun evaluate(request: Request, throwable: Throwable) = policy(request, throwable)
