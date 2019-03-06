340d3b00a3d7bcc55460f012c937e6287f8f5581
Ilya Lim <ilya.lim@agoda.com>
2019-03-06 18:15:07 +0700

    Add documentation for ConverterFactories

--- core/src/main/kotlin/com/agoda/ninjato/converter/ConverterFactories.kt
+++ core/src/main/kotlin/com/agoda/ninjato/converter/ConverterFactories.kt
@@ -1,15 +1,33 @@
 package com.agoda.ninjato.converter
 
+/**
+ * DSL context for aggregating [converter factories][BodyConverter.Factory].
+ * Supports additions of single factory and list of factories.
+ *
+ * When resolving all the aggregations across the DSL cascade, keep in mind,
+ * that factories that were defined on the lowest level will come first.
+ * This is done to support overriding of converters.
+ */
 class ConverterFactories {
     @PublishedApi
     internal var parent: ConverterFactories? = null
 
     private val added: MutableList<BodyConverter.Factory> = mutableListOf()
 
+    /**
+     * Adds provided factory to the aggregation.
+     *
+     * @param factory factory to add
+     */
     operator fun plusAssign(factory: BodyConverter.Factory) {
         added.add(factory)
     }
 
+    /**
+     * Adds provided factories to the aggregation.
+     *
+     * @param factories list of factories to add
+     */
     operator fun plusAssign(factories: List<BodyConverter.Factory>) {
         added.addAll(factories)
     }
