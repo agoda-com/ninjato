56064ce40df9d6f1ba34f8731c31e2fd3c742195
Dennis <dennis.hsieh@agoda.com>
2019-03-07 14:29:01 +0700

    Switch to crossinline.

--- extensions/extension-rxjava/src/main/kotlin/com/agoda/ninjato/extensions/rxjava/Apis.kt
+++ extensions/extension-rxjava/src/main/kotlin/com/agoda/ninjato/extensions/rxjava/Apis.kt
@@ -5,8 +5,8 @@ import rx.Completable
 import rx.Observable
 import rx.Single
 
-inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.completable(crossinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
 
-inline fun <A : Api, reified T> A.single(noinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.single(crossinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
 
-inline fun <A : Api, reified T> A.observable(noinline receiver: A.() -> T): Observable<T> = Observable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.observable(crossinline receiver: A.() -> T): Observable<T> = Observable.fromCallable { receiver() }
--- extensions/extension-rxjava2/src/main/kotlin/com/agoda/ninjato/extensions/rxjava2/Apis.kt
+++ extensions/extension-rxjava2/src/main/kotlin/com/agoda/ninjato/extensions/rxjava2/Apis.kt
@@ -6,10 +6,12 @@ import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.Single
 
-inline fun <A : Api, reified T> A.completable(noinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.completable(crossinline receiver: A.() -> Unit): Completable = Completable.fromCallable { receiver() }
 
-inline fun <A : Api, reified T> A.single(noinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.single(crossinline receiver: A.() -> T): Single<T> = Single.fromCallable { receiver() }
 
-inline fun <A : Api, reified T> A.observable(noinline receiver: A.() -> T): Observable<T> = Observable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.observable(crossinline receiver: A.() -> T): Observable<T> = Observable.fromCallable { receiver() }
 
-inline fun <A : Api, reified T> A.flowable(noinline receiver: A.() -> T): Flowable<T> = Flowable.fromCallable { receiver() }
+inline fun <A : Api, reified T> A.flowable(crossinline receiver: A.() -> T): Flowable<T> = Flowable.fromCallable { receiver() }
+
+
