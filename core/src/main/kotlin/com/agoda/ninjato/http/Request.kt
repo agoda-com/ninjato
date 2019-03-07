98310ca3e199b746986b74dd8cd87789d0736aa8
Ilya Lim <ilya.lim@agoda.com>
2019-03-07 16:03:21 +0700

    Add docs for Request and Response

--- core/src/main/kotlin/com/agoda/ninjato/http/Request.kt
+++ core/src/main/kotlin/com/agoda/ninjato/http/Request.kt
@@ -6,6 +6,10 @@ import com.agoda.ninjato.policy.FallbackPolicy
 import com.agoda.ninjato.policy.RetryPolicy
 import com.agoda.ninjato.converter.ConverterFactories
 
+/**
+ * Base http entity of the library.
+ * Translates into the actual http request by [HttpClient].
+ */
 open class Request {
     lateinit var method: Method
 
@@ -13,6 +17,12 @@ open class Request {
     var endpointUrl: String = ""
     var fullUrl: String = ""
 
+    /**
+     * Represents the actual URL that the server needs to hit.
+     * If [fullUrl] property is provided, than this property will return value of [fullUrl]
+     * or else will return concatenation of [baseUrl] (provided by [api][com.agoda.ninjato.Api]) and
+     * [endpointUrl].
+     */
     val url: String
         get() = if (fullUrl.isNotBlank()) fullUrl else baseUrl + endpointUrl
 
@@ -21,10 +31,18 @@ open class Request {
 
     var retries = 0
 
+    /**
+     * Factory class for creating instances of request.
+     * In case you need to extend [Request] and enrich it with some data of your own,
+     * provide instance of factory to [HttpClient].
+     */
     abstract class Factory {
         abstract fun create(): Request
     }
 
+    /**
+     * DSL context for configuring the [Request].
+     */
     open class Configurator : Commons {
         override val headers = Headers()
         override val interceptors = Interceptors()
@@ -43,7 +61,10 @@ open class Request {
             it.headers.putAll(headers.resolve())
         }
 
-        class WithBody : Configurator() {
+        /**
+         * DSL context for configuring the [Request] with [Body]
+         */
+        open class WithBody : Configurator() {
             var body: Any? by Body.Delegate(converterFactories)
             override fun configure(instance: Request) = super.configure(instance).also { it.body = body as Body? }
         }
--- core/src/main/kotlin/com/agoda/ninjato/http/Response.kt
+++ core/src/main/kotlin/com/agoda/ninjato/http/Response.kt
@@ -1,5 +1,9 @@
 package com.agoda.ninjato.http
 
+/**
+ * Base http entity of the library.
+ * [HttpClient] translates it's internal response entities into this one and propagates further.
+ */
 open class Response {
     lateinit var request: Request
 
@@ -10,6 +14,11 @@ open class Response {
     val isSuccess: Boolean
         get() = code == 200
 
+    /**
+     * Factory class for creating instances of request.
+     * In case you need to extend [Response] and enrich it with some data of your own,
+     * provide instance of factory to [HttpClient].
+     */
     abstract class Factory {
         abstract fun create(): Response
     }
